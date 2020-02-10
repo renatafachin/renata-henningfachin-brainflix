b9c4b2fc800ab03ccc12f68eac47dddce73ac4c7
Renata Fachin <renatafachin@gmail.com>
2020-02-10 11:31:41 -0800

    Starting Brainflix

---
+++ sprint-one/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+
+    </div>
+  )
+}
